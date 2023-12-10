87d6bdd8f1ce2f28a3878a36220d10fe29b7da8a
SanderLeivannOlsen <sandleivolse@gmail.com>
2023-12-10 10:28:51 +0100

    søppel2

---
+++ nettavis/javascript/scripts.js
@@ -0,0 +1,13 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const overlay = document.querySelector(".overlay");
+    const hero = document.querySelector(".hero");
+    
+    window.addEventListener("scroll", function() {
+      const scrollPosition = window.scrollY;
+      
+      overlay.style.transform = `translateY(${scrollPosition * 0.2}px)`;
+
+      hero.style.transform = `translateY(${scrollPosition * 0.4}px)`;
+    });
+  });
