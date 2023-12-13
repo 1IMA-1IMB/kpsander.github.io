7749ab2abb042a86af055b609ea21bb30d524113
SanderLeivannOlsen <sandleivolse@gmail.com>
2023-12-13 10:42:01 +0100

    nettavis_innlevering

--- nettavis/javascript/scripts.js
+++ nettavis/javascript/scripts.js
@@ -1,13 +1,33 @@
-
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const overlay = document.querySelector(".overlay");
     const hero = document.querySelector(".hero");
+    const hero_overlay_img = document.querySelector(".hero_overlay_img");
     
     window.addEventListener("scroll", function() {
       const scrollPosition = window.scrollY;
       
-      overlay.style.transform = `translateY(${scrollPosition * 0.2}px)`;
+      overlay.style.transform = `translateY(${scrollPosition * 0.15}px)`;
 
-      hero.style.transform = `translateY(${scrollPosition * 0.4}px)`;
+      hero.style.transform = `translateY(${scrollPosition * 0.35}px)`;
+
+      hero_overlay_img.style.transform = `translateY(${scrollPosition * 0.17}px)`;
     });
   });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    const hiddenElements = document.querySelectorAll('.hidden');
+  
+    hiddenElements.forEach((el) => observer.observe(el));
+  
+    console.log(hiddenElements);
+});
